6a4b1ddf4ebab43e1b121e34f10f13e2800c2888
Youssef <youssefiptv936@gmail.com>
2026-04-20 15:56:40 -0700

    تعديل النصوص وتحسين التصميم للهاتف

--- src/app/components/HtmlTools/H1.tsx
+++ src/app/components/HtmlTools/H1.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function H1({ children }: Props) {
   return (
-    <h1 className=" text-6xl font-black text-center leading-relaxed text-slate-200  md:w-[80%] bg-amber-300  ">
+    <h1 className=" text-4xl font-black text-center leading-relaxed text-slate-200  md:w-[80%]   ">
       {""}
       {children}
       {""}
